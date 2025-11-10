0e4c3fe0e9521f2121fb023249153d8a5ba62807
FilipeDawra <filipedawra@gmail.com>
2025-11-10 14:54:30 -0300

    Operadores Aritiméticos

---
+++ OperadoresAritimeticos.java
@@ -0,0 +1,28 @@
+package fundamentos.operadores;
+
+public class OperadoresAritimeticos {
+	public static void main(String[] args) {
+		
+		double x = 17;
+		double y = 15;
+		
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+		System.out.println(x % y);
+		System.out.println("_______________");
+		
+		int a = 17;
+		int b = 15;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a / (double)b);
+		System.out.println(a % b);
+			
+	}
+
+}
