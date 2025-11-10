e73232e6b7918de6bdbb346adab0c0cccf9a7fd7
FilipeDawra <filipedawra@gmail.com>
2025-11-10 14:57:22 -0300

    Operadores de Atribuição

---
+++ OperadoresAtribuição.java
@@ -0,0 +1,16 @@
+package fundamentos.operadores;
+
+public class OperadoresAtribuição {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = a;
+		int c = a + b;
+		
+		c += b; //c = c + b;
+		c -= b; //c = c - b;
+		c *= b; //c = c + b;
+		c /= b; //c = c + b;
+		c %= 2; //c = c % 2;
+	}
+
+}
